6fdd420fd0ab534a2df29a654c109e8d1225bc0c
Janhavi-240307 <janhavi746578@gmail.com>
2025-08-28 09:15:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const bars = document.querySelector(".fa-bars");
 cartIcon.addEventListener("click", () => cartTab.classList.add('cart-tab-active'));
 closebtn.addEventListener("click", () => cartTab.classList.remove('cart-tab-active'));
 hamburger.addEventListener("click", () => mobileMenu.classList.toggle('mobile-menu-active'));
-hamburger.addEventListener("click", () => bars.classList.toggle('fa-xmark'));
+
 
 let productList = [];
 let cartProd = [];
